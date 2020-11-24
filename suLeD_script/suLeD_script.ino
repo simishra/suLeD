73dacfbeaf64e5c83f1c17d3025981694a9a3424
owner <simivegan@gmail.com>
2020-11-24 18:34:14 +0100

    added runninglight script

--- suLeD_script/suLeD_script.ino
+++ suLeD_script/suLeD_script.ino
@@ -1,12 +1,13 @@
 #include <FastLED.h>
+#define NUM_LEDS 60
+#define DATA_PIN 2
 
-
-
-
+CRGB leds[NUM_LEDS];
 int ledPin = 2;
 
 void setup() {
   // put your setup code here, to run once:
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
   pinMode(ledPin, OUTPUT);
   Serial.begin(115200);
 }
@@ -14,4 +15,21 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(ledPin, HIGH);
+}
+
+void RunningLights(byte red, byte green, byte blue, int WaveDelay) {
+  int Position=0;
+ 
+  for(int j=0; j<NUM_LEDS*2; j++)
+  {
+      Position++; 
+      for(int i=0; i<NUM_LEDS; i++) {
+        setPixel(i,((sin(i+Position) * 127 + 128)/255)*red,
+                   ((sin(i+Position) * 127 + 128)/255)*green,
+                   ((sin(i+Position) * 127 + 128)/255)*blue);
+      }
+     
+      showStrip();
+      delay(WaveDelay);
+  }
 }
