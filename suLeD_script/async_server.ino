eb5e59c4e6eb0e95edddcc1b51a9fcba162452b4
moorhexa <hajo@zingelxuc.de>
2020-11-25 17:58:00 +0100

    idk

--- suLeD_script/async_server.ino
+++ suLeD_script/async_server.ino
@@ -1,16 +1,34 @@
+int red = 123;
+int green = 33;
+int blue = 5;
+int Wave_Delay = 500;
+//index html to show overview of functions
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+  <head>
+    <title>UtzAtz Disco Disco</title>
+  </head>
+  <body>
+    hallo<br>
+    tschuess
+  </body>
+</html>)rawliteral";
+
 void notFound(AsyncWebServerRequest *request){
   request->send(404, "text/plain", "404 Not found");
 }
 
+
 void setupServer(){
-  server.onNotFound(notFound);
-  server.begin();
-  server.on("/RunngingLights/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/html", "running lights");
-    RunningLights( 124,  23,  45,  500);
+  server.on("/RunngingLights", HTTP_GET, [](AsyncWebServerRequest *request){
+    RunningLights(red, green, blue, Wave_Delay);
+    request->send_P(200, "text/html", "running lights");
   });
   server.on("/sos", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/html", "sos");
     morse();
+    request->send_P(200, "text/html", "sos");
   });
+  
+  server.onNotFound(notFound);
+  server.begin();
 }
--- suLeD_script/wifi_script.ino
+++ suLeD_script/wifi_script.ino
@@ -9,16 +9,3 @@ void initWiFi(){
   Serial.println("IP Address: ");
   Serial.println(WiFi.localIP()); 
 }
-
-//void setupClient(){
-//  WiFiClient client = server.available();
-
-//  if (client){
-//    String req = client.readStringUntil('\r');
-//    client.flush();
-//    if(req.indexOf("/led/on") != -1)
-//    digitalWrite(ledPin, HIGH);
-//    if(req.indexOf("/led/off") != -1)
-//    digitalWrite(ledPin, LOW);
-//  }
-//}
